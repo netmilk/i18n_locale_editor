a915cf2d0d4a65c8e6da7f7f8fd347dffdc5fef6
Adam Kliment <adam.kliment@virtualmaster.cz>
2011-03-07 14:39:02 +0100

    readme mods, assets copying

--- rails/init.rb
+++ rails/init.rb
@@ -1,5 +1,12 @@
 require 'hash_tools'
 require 'translate_monkey_patch'
+require 'ftools'
+
+if ENV['I18N_EDITOR']== "true"
+  src = File.join(directory,'public',"i18n-locale-editor.js")
+  dst = File.join(RAILS_ROOT,'public', 'javascripts')
+  File.copy(src,dst)
+end
 
 %w{ models controllers helpers }.each do |dir|
   path = File.join(directory, 'app',dir)
@@ -10,3 +17,4 @@ end
 
 require File.join(directory, "app", "helpers", "translations_helper.rb")
 ActionView::Base.send :include, TranslationsHelper;
+
