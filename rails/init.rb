cf15138a98c9d0a8b6ffc0f77531929e68d5cc42
Adam Kliment <adam.kliment@virtualmaster.cz>
2011-03-07 19:25:01 +0100

    fixed issue #1

--- rails/init.rb
+++ rails/init.rb
@@ -3,13 +3,13 @@ require 'translate_monkey_patch'
 require 'ftools'
 
 if ENV['I18N_EDITOR']== "true"
-  src = File.join(directory,'public',"i18n-locale-editor.js")
+  src = File.join(File.dirname(__FILE__),'public',"i18n-locale-editor.js")
   dst = File.join(RAILS_ROOT,'public', 'javascripts')
   File.copy(src,dst)
 end
 
 %w{ models controllers helpers }.each do |dir|
-  path = File.join(directory, 'app',dir)
+  path = File.join(File.dirname(__FILE__), 'app',dir)
   $LOAD_PATH << path
   ActiveSupport::Dependencies.load_paths << path
   ActiveSupport::Dependencies.load_once_paths.delete(path)
